4f5ccbd02586c5dade8315c830f8423d8580e65e
JohannesGuss <Johannes.Gussenbauer@gmail.com>
2017-03-13 11:51:48 +0000

    adsfg

--- cedefop.R
+++ cedefop.R
@@ -1,10 +1,15 @@
 library(data.table)
 setwd("/data")
-csk <- fread("cedefop/ft_skill_profession_en.csv",header=FALSE)
-setnames(csk,colnames(csk),c("GeneralId","PublicationCountry","Esco_Level_4","Esco_Level_3",
+prof <- fread("cedefop/ft_skill_profession_en.csv",header=FALSE)
+setnames(prof,colnames(csk),c("GeneralId","PublicationCountry","Esco_Level_4","Esco_Level_3",
                              "Esco_Level_2","Esco_Level_1","Skill_Esco_Level_4","Skill_Esco_Level_3",
                              "Skill_Esco_Level_2","Skill_Esco_Level_1","Skill_Esco_Level_0"))
-cskr <- fread("cedefop/ft_skill_profession_requirement_en.csv")
+
+expi <- fread("cedefop/ft_skill_profession_experience_en.csv",header=FALSE)
+prof_new <- fread("cedefop/ft_skill_profession_new_en.csv",header=FALSE)
+requ <- fread("cedefop/ft_skill_profession_requirement_en.csv",header=FALSE)
+
+colnames(expi) <- colnames(prof_new) <- colnames(requ) <- c("GeneralId","PublicationCountry","Esco_Level_4","Esco_Level_3","Esco_Level_2","Esco_Level_1","IdEsco_Skill","NGram") 
 
 
 doc <- fread("cedefop/ft_document_en.csv",header=FALSE)
