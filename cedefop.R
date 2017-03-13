55a78638b35a14ae26d97ad57209dcaca7822bef
JohannesGuss <Johannes.Gussenbauer@gmail.com>
2017-03-13 13:10:21 +0000

    swefthgg

--- cedefop.R
+++ cedefop.R
@@ -1,20 +1,24 @@
 library(data.table)
-setwd("/data")
-prof <- fread("cedefop/ft_skill_profession_en.csv",header=FALSE)
-setnames(prof,colnames(csk),c("GeneralId","PublicationCountry","Esco_Level_4","Esco_Level_3",
+
+prof <- fread("/data/cedefop/ft_skill_profession_en.csv",header=FALSE)
+setnames(prof,colnames(prof),c("GeneralId","PublicationCountry","Esco_Level_4","Esco_Level_3",
                              "Esco_Level_2","Esco_Level_1","Skill_Esco_Level_4","Skill_Esco_Level_3",
                              "Skill_Esco_Level_2","Skill_Esco_Level_1","Skill_Esco_Level_0"))
 
-expi <- fread("cedefop/ft_skill_profession_experience_en.csv",header=FALSE)
-prof_new <- fread("cedefop/ft_skill_profession_new_en.csv",header=FALSE)
-requ <- fread("cedefop/ft_skill_profession_requirement_en.csv",header=FALSE)
+expi <- fread("/data/cedefop/ft_skill_profession_experience_en.csv",header=FALSE)
+prof_new <- fread("/data/cedefop/ft_skill_profession_new_en.csv",header=FALSE)
+requ <- fread("/data/cedefop/ft_skill_profession_requirement_en.csv",header=FALSE)
 
 colnames(expi) <- colnames(prof_new) <- colnames(requ) <- c("GeneralId","PublicationCountry","Esco_Level_4","Esco_Level_3","Esco_Level_2","Esco_Level_1","IdEsco_Skill","NGram") 
 
 
-doc <- fread("cedefop/ft_document_en.csv",header=FALSE)
+doc <- fread("/data/cedefop/ft_document_en.csv",header=FALSE)
 setnames(doc,colnames(doc),c("GeneralId","PublicationCountry","GrabDate","YearGrabDate","MonthGrabDate",
                              "DayGrabDate","ExpireDate","YearExpireDate","MonthExpireDate","DayExpireDate",
                              "Esco_Level_4","Esco_Level_3","Esco_Level_2","Esco_Level_1","Nut_Level_3",
                              "Nut_Level_2","Nut_Level_1","Nut_Level_0","Contract","EducationalLevel","Industry_Level_2",
                              "Industry_Level_1","WorkingHours"))
+
+
+
+
--- eures.R
+++ eures.R
@@ -1,23 +1,24 @@
 library(VIM);library(data.table);
 
-person <- fread("eures/CV data DWH 2016-12-01 CSV/CV_MAIN.csv")
-res_int <- fread("eures/CV data DWH 2016-12-01 CSV/CV_RESIDENCE_INTER.csv")
-res <- fread("eures/CV data DWH 2016-12-01 CSV/CV_RES_COUNTRY.csv")
-lo_int <- fread("eures/CV data DWH 2016-12-01 CSV/CV_LAST_OCCUPATION_INTER.csv")
-lo <- fread("eures/CV data DWH 2016-12-01 CSV/CV_LAST_OCCUPATION.csv")
-sk <- fread("eures/CV data DWH 2016-12-01 CSV/CV_SKILL.csv")
+
+person <- fread("/data/eures/CV data DWH 2016-12-01 CSV/CV_MAIN.csv")
+res_int <- fread("/data/eures/CV data DWH 2016-12-01 CSV/CV_RESIDENCE_INTER.csv")
+res <- fread("/data/eures/CV data DWH 2016-12-01 CSV/CV_RES_COUNTRY.csv")
+lo_int <- fread("/data/eures/CV data DWH 2016-12-01 CSV/CV_LAST_OCCUPATION_INTER.csv")
+lo <- fread("/data/eures/CV data DWH 2016-12-01 CSV/CV_LAST_OCCUPATION.csv")
+sk <- fread("/data/eures/CV data DWH 2016-12-01 CSV/CV_SKILL.csv")
 person <- merge(person,res_int,by="ID",all.x=TRUE,all.y=FALSE)
 person <- merge(person, res, by="RES_COUNTRY_ID",all.x=T,all.y=FALSE)
 g <- person[RES_COUNTRY=="Germany"]
-ed <- fread("eures/CV data DWH 2016-12-01 CSV/CV_EDUCATION.csv")
+ed <- fread("/data/eures/CV data DWH 2016-12-01 CSV/CV_EDUCATION.csv")
 g <- merge(g,ed,all.x=TRUE,by="ID")
 g <- merge(g,sk,by="ID",all.x=TRUE,all.y=FALSE)
 g <- merge(g,lo_int,by="ID",all.x=T)
 g <- merge(g,lo,by="OCCUPATION_ID",all.x=T)
 
-job <- fread("eures/JV data DWH 2016-11-30 CSV/JV_MAIN.csv")
-edu <- fread("eures/JV data DWH 2016-11-30 CSV/JV_EDUC_SKILLS.csv")
-isco <- fread("eures/JV data DWH 2016-11-30 CSV/JV_ISCO.csv")
+job <- fread("/data/eures/JV data DWH 2016-11-30 CSV/JV_MAIN.csv")
+edu <- fread("/data/eures/JV data DWH 2016-11-30 CSV/JV_EDUC_SKILLS.csv")
+isco <- fread("/data/eures/JV data DWH 2016-11-30 CSV/JV_ISCO.csv")
 isco[,ISCO_SYS_CODE:=as.character(ISCO_SYS_CODE)]
 isco <- isco[!duplicated(ISCO_SYS_CODE)]
 job <- merge(job,edu,by="EDU_SKILLS",all.x=TRUE,all.y=FALSE)
