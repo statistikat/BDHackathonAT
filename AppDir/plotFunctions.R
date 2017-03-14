de67ef2a336af06bdc8d70a403da0c910831b2d3
marleneweinauer <marlene.weinauer@gmx.at>
2017-03-14 19:47:34 +0000

    plotly

--- AppDir/plotFunctions.R
+++ AppDir/plotFunctions.R
@@ -5,15 +5,16 @@ library(plotly)
 #    geom_point()+theme_bw()+scale_colour_gradient2()+theme(legend.position = "none")
 #}
 
-# änderungen zu plotMain: rot/grün; scale_size; achsenbeschriftung
 plotMain <- function(bubbleData){
-  ggplot(bubbleData,aes(x=quarter,y=Jobgroup,size=value,color=pressure))+
+  g <- ggplot(bubbleData,aes(x=quarter,y=Jobgroup,size=value,color=pressure))+
     geom_point()+theme_bw()+scale_colour_gradient(low = "indianred3", high = "lightsteelblue")+theme(legend.position = "none")+ 
     scale_size_continuous(range=c(8,14)) + # größen einstellen wie wir es brauchen 
     theme(axis.text.x = element_text(size=13), 
           axis.text.y = element_text(size=13), 
           axis.title.x= element_text(size=15)) +
     ylab("") #+
+  ggplotly(g)
+  #g
   # coord_fixed(ratio = 1) #verändert abstand der quartale
 }
 
@@ -43,7 +44,7 @@ plotBar <- function(skillmiss, jobgruppe){ #bei jobgruppe namen eingeben
   dat$SKILL <- factor(dat$SKILL, levels = dat$SKILL)
   dat$sign <- ifelse(dat$availability >= 1, "positive", "negative")
   
-  ggplot(data=dat, aes(x=SKILL, y=N, fill=sign)) +
+  g <- ggplot(data=dat, aes(x=SKILL, y=N, fill=sign)) +
     geom_bar(colour="black", stat="identity") +
     guides(fill=FALSE) +
     scale_fill_manual(values = c("positive" = "lightsteelblue", "negative" = "indianred3"))+
@@ -53,6 +54,8 @@ plotBar <- function(skillmiss, jobgruppe){ #bei jobgruppe namen eingeben
           axis.title.x= element_text(size=15)) +
     ylab("") +
     scale_x_discrete(labels = function(x) str_wrap(x, width = 15))
+  #ggplotly(g)
+  g
 }
 
 
