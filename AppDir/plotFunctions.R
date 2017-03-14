f7a04e59a25ebc583e4ed5ee80466683f14ededa
marleneweinauer <marlene.weinauer@gmx.at>
2017-03-14 16:38:59 +0000

    diverses

--- AppDir/plotFunctions.R
+++ AppDir/plotFunctions.R
@@ -1,15 +1,17 @@
-plotMain <- function(bubbleData){
-  ggplot(bubbleData,aes(x=quarter,y=Jobgroup,size=value,color=pressure))+
-    geom_point()+theme_bw()+scale_colour_gradient2()+theme(legend.position = "none")
-}
+library(stringr)
+#plotMain <- function(bubbleData){
+#  ggplot(bubbleData,aes(x=quarter,y=Jobgroup,size=value,color=pressure))+
+#    geom_point()+theme_bw()+scale_colour_gradient2()+theme(legend.position = "none")
+#}
 
 # änderungen zu plotMain: rot/grün; scale_size; achsenbeschriftung
-plotMain2 <- function(bubbleData){
+plotMain <- function(bubbleData){
   ggplot(bubbleData,aes(x=quarter,y=Jobgroup,size=value,color=pressure))+
     geom_point()+theme_bw()+scale_colour_gradient(low = "red", high = "green")+theme(legend.position = "none")+ 
     scale_size_continuous(range=c(8,14)) + # größen einstellen wie wir es brauchen 
-    theme(axis.text.x = element_text(size=10), 
-          axis.text.y = element_text(size=10)) +
+    theme(axis.text.x = element_text(size=13), 
+          axis.text.y = element_text(size=13), 
+          axis.title.x= element_text(size=15)) +
     ylab("") #+
   # coord_fixed(ratio = 1) #verändert abstand der quartale
 }
@@ -23,11 +25,13 @@ plotBar <- function(skillmiss, jobgruppe){ #bei jobgruppe namen eingeben
     geom_bar(colour="black", stat="identity") +
     guides(fill=FALSE) +
     scale_fill_gradient(low = "red", high = "green")+
-    ggtitle("Top 5 Skills") +
-    theme(axis.text.x = element_text(size=11), 
-          axis.text.y = element_text(size=11), 
-          plot.title=element_text(size=14,face="bold")) +
-    ylab("")
+    theme_bw()+
+    theme(axis.text.x = element_text(size=13), 
+          axis.text.y = element_text(size=13), 
+          axis.title.x= element_text(size=15)) +
+    ylab("") +
+    scale_x_discrete(labels = function(x) str_wrap(x, width = 15))
+    
 }
 
 
