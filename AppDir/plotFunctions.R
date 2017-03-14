bb2c244dc19aa0aedc04d804c4d6909c2265946c
marleneweinauer <marlene.weinauer@gmx.at>
2017-03-14 17:46:56 +0000

    Farbenangleich

--- AppDir/plotFunctions.R
+++ AppDir/plotFunctions.R
@@ -1,4 +1,5 @@
 library(stringr)
+library(plotly)
 #plotMain <- function(bubbleData){
 #  ggplot(bubbleData,aes(x=quarter,y=Jobgroup,size=value,color=pressure))+
 #    geom_point()+theme_bw()+scale_colour_gradient2()+theme(legend.position = "none")
@@ -7,7 +8,7 @@ library(stringr)
 # änderungen zu plotMain: rot/grün; scale_size; achsenbeschriftung
 plotMain <- function(bubbleData){
   ggplot(bubbleData,aes(x=quarter,y=Jobgroup,size=value,color=pressure))+
-    geom_point()+theme_bw()+scale_colour_gradient(low = "red", high = "green")+theme(legend.position = "none")+ 
+    geom_point()+theme_bw()+scale_colour_gradient(low = "indianred3", high = "lightsteelblue")+theme(legend.position = "none")+ 
     scale_size_continuous(range=c(8,14)) + # größen einstellen wie wir es brauchen 
     theme(axis.text.x = element_text(size=13), 
           axis.text.y = element_text(size=13), 
@@ -17,21 +18,41 @@ plotMain <- function(bubbleData){
 }
 
 
+# plotBar: Farbabstufungen 
+#plotBar <- function(skillmiss, jobgruppe){ #bei jobgruppe namen eingeben
+#  dat <- skillmiss[which(skillmiss$job_groups==jobgruppe),]
+#  dat$SKILL <- factor(dat$SKILL, levels = dat$SKILL)
+#  
+#  ggplot(data=dat, aes(x=SKILL, y=N, fill=availability)) +
+#    geom_bar(colour="black", stat="identity") +
+#    guides(fill=FALSE) +
+ #   scale_fill_gradient(low = "coral1", high = "seagreen1")+
+#    theme_bw()+
+#    theme(axis.text.x = element_text(size=13), 
+#          axis.text.y = element_text(size=13), 
+#          axis.title.x= element_text(size=15)) +
+#    ylab("") +
+#    scale_x_discrete(labels = function(x) str_wrap(x, width = 15))
+#    
+#}
+
+
+# plotBar: nur 2 farben
 plotBar <- function(skillmiss, jobgruppe){ #bei jobgruppe namen eingeben
   dat <- skillmiss[which(skillmiss$job_groups==jobgruppe),]
   dat$SKILL <- factor(dat$SKILL, levels = dat$SKILL)
+  dat$sign <- ifelse(dat$availability >= 1, "positive", "negative")
   
-  ggplot(data=dat, aes(x=SKILL, y=N, fill=availability)) +
+  ggplot(data=dat, aes(x=SKILL, y=N, fill=sign)) +
     geom_bar(colour="black", stat="identity") +
     guides(fill=FALSE) +
-    scale_fill_gradient(low = "red", high = "green")+
+    scale_fill_manual(values = c("positive" = "lightsteelblue", "negative" = "indianred3"))+
     theme_bw()+
     theme(axis.text.x = element_text(size=13), 
           axis.text.y = element_text(size=13), 
           axis.title.x= element_text(size=15)) +
     ylab("") +
     scale_x_discrete(labels = function(x) str_wrap(x, width = 15))
-    
 }
 
 
